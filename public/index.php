716b330634b207f5cf5e3b97ea8bc046a58d688b
Michael Griffiths <mdhgriffiths@gmail.com>
2016-05-30 00:06:26 -0700

    Added public/index.php

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AdoptOrNot\Api;
+
+// Include core bootstrap file
+require_once '../src/bootstrap.php';
+
+// TODO: application code
+$json = 'Hello World!';
+echo json_encode($json);
