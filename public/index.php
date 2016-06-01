baa1c77b0029242a7d8d285ae35c14f757351e3b
Michael Griffiths <mdhgriffiths@gmail.com>
2016-05-31 18:07:55 -0700

    Added Slim\App and setup / route to return JSON

--- public/index.php
+++ public/index.php
@@ -1,10 +1,38 @@
 <?php
 
 namespace AdoptOrNot\Api;
+use \Slim\App as SlimApp;
 
 // Include core bootstrap file
 require_once '../src/bootstrap.php';
 
-// TODO: application code
-$json = 'Hello World!';
-echo json_encode($json);
+// Setup application
+$app = new SlimApp([
+    'settings' => [
+        // TODO: only set in DEV env
+        'displayErrorDetails' => true,
+        'database' => [
+            'hostname' => env('ADOPTORNOT_MYSQL_HOSTNAME', 'localhost'),
+            'username' => env('ADOPTORNOT_MYSQL_USERNAME'),
+            'password' => env('ADOPTORNOT_MYSQL_PASSWORD'),
+            'database' => env('ADOPTORNOT_MYSQL_DATABASE')
+        ]
+    ]
+]);
+
+// Add dependencies
+$container = $app->getContainer();
+$container['db'] = function ($c) {
+    $config = $c['settings']['db.connection'];
+    $dsn = sprintf('mysql:host=%s;dbname=%s', $config['hostname'], $config['database']);
+    $pdo = new \PDO($dsn, $config['username'], $config['password']);
+    return $pdo;
+};
+
+// Main application route
+$app->get('/', function ($request, $response) {
+    return $response->withJson('Hello World!');
+});
+
+// Execute
+$app->run();
