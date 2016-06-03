590fe4409bcc0fbaad7acdebcc1fab0d2336c99e
Michael <Michael>
2016-06-02 23:37:15 -0700

    Removed PDO/MySQL dependency (not needed yet)

--- README.md
+++ README.md
@@ -5,12 +5,6 @@ REST/JSON API for Adopt or Not, powered by [Adoptable Pet Data from RescueGroups
 An `.env` file must be created to store the application's config:
 
 ```
-# MySQL Connection Settings
-ADOPTORNOT_MYSQL_HOSTNAME=
-ADOPTORNOT_MYSQL_USERNAME=
-ADOPTORNOT_MYSQL_PASSWORD=
-ADOPTORNOT_MYSQL_DATABASE=
-
 # RescueGroups.org API Key
 RESCUEGROUPS_APIKEY={XXXX}
 ```
--- src/container.php
+++ src/container.php
@@ -5,14 +5,6 @@ namespace AdoptOrNot\Api;
 // ContainerInterface
 $container = $app->getContainer();
 
-// PDO database connection
-$container['db'] = function ($c) {
-    $config = $c['settings']['db.connection'];
-    $dsn = sprintf('mysql:host=%s;dbname=%s', $config['hostname'], $config['database']);
-    $pdo = new \PDO($dsn, $config['username'], $config['password']);
-    return $pdo;
-};
-
 // RescueGroup API client
 $container['api'] = function ($c) {
     $apiKey = env('RESCUEGROUPS_APIKEY');
